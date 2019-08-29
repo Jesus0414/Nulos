3795c3f7288e52ad3a927b634581d92f18750d2f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-29 12:38:42 -0700

    Agregar archivos de proyecto.

---
+++ Nulos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nulos
+{
+    class Program
+    {
+        static Alumno alumno;
+        static void Main(string[] args)
+        {
+            alumno = new Alumno();
+            //alumno.Materias.Add("Matematicas");
+            imprimirAlumno();
+            Console.Read();
+        }
+        static void imprimirAlumno()
+        {
+            try
+            {
+                alumno.Nombre = "Jesús";
+                Console.WriteLine("Nombre: " + alumno.Nombre);
+                //alumno = null;
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
